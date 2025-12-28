3931b809b64c1e31f9562e3ab85b3205121b90b4
Aidil-Nov <lomantp537.com>
2025-12-28 22:44:43 +0700

    feat(api): add report endpoint, storage link, and fix mass assignment

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+    use HasFactory;
+
+    // TAMBAHKAN BAGIAN INI:
+    protected $fillable = [
+        'user_id',
+        'ticket_id',
+        'category',
+        'urgency',
+        'description',
+        'location_address',
+        'latitude',
+        'longitude',
+        'photo_url',
+        'status',
+        'admin_note'
+    ];
 }
--- database/seeders/CampaignSeeder.php
+++ database/seeders/CampaignSeeder.php
@@ -13,7 +13,7 @@ class CampaignSeeder extends Seeder
             [
                 'title' => 'Banjir Bandang Demak',
                 'description' => 'Ribuan warga terdampak banjir bandang. Mereka membutuhkan makanan, selimut, dan obat-obatan segera.',
-                'image_url' => 'https://asset.kompas.com/crops/O4oW6jO9p7x5ZJ9z6xW5ZJ9z6xW=/0x0:1000x667/750x500/data/photo/2024/02/12/65c9b9b9b9b9b.jpg', // Ganti link gambar jika perlu
+                'image_url' => 'http://172.20.10.6:8000/images/banjir.jpg', // Ganti link gambar jika perlu
                 'target_amount' => 500000000,
                 'collected_amount' => 125000000,
                 'organizer' => 'BPBD Demak',
@@ -24,7 +24,7 @@ class CampaignSeeder extends Seeder
             [
                 'title' => 'Gempa Cianjur Bangkit',
                 'description' => 'Bantu pembangunan kembali sekolah darurat untuk anak-anak korban gempa.',
-                'image_url' => 'https://asset.kompas.com/crops/example.jpg',
+                'image_url' => 'http://172.20.10.6:8000/images/gempa.jpg',
                 'target_amount' => 200000000,
                 'collected_amount' => 190000000,
                 'organizer' => 'Relawan Kita Pusat',
